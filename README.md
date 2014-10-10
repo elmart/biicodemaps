0674de3a86830cf9e37ae9dab3d92a884acaa8fd
Eliseo Martínez <eliseomarmol@gmail.com>
2014-10-10 10:21:04 +0200

    Initial project structure.

---
+++ README.md
@@ -0,0 +1,7 @@
+# BiiCodeMaps
+
+A little utility to deal with routing problems on a given map.
+
+Maps are described through cities and roads, where cities have cartesian
+coordinates, and roads connect two cities bidirectionally.
+Roads are supposed straight.
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+
+setup(name='biicodemaps',
+      version='0.1',
+      description='Map routing utilities',
+      author='Eliseo Martínez',
+      author_email='eliseomarmol@gmail.com',
+      packages=['biicodemaps'])
