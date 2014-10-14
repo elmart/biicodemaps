ead474b53cef10382adcd77b00c7e13998c475f0
Eliseo Martínez <eliseomarmol@gmail.com>
2014-10-14 09:34:05 +0200

    Infrastructure: Integrate pytest with setuptools.

--- setup.py
+++ setup.py
@@ -1,14 +1,43 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-
-from distutils.core import setup
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
 from version import version
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(name='biicodemaps',
       version=version,
-      description='Map routing utilities',
+      description='A small utility to deal with map routing problems.',
+
+      packages=['biicodemaps'],
+      install_requires=['docopt'],
+      tests_require=['pytest'],
+
+      cmdclass={'test': PyTest},
+      include_package_data=True,
+
       author='Eliseo Martínez',
       author_email='eliseomarmol@gmail.com',
-      packages=['biicodemaps'],
+
       entry_points={'console_scripts': ['bcm = biicodemaps.tool:main']})
